845206e897613addec0a7dc677bd9fdfb130673a
dhirajkmr38 <42024660+dhirajkmr38@users.noreply.github.com>
2018-08-02 12:47:03 +0530

    abc
    skamn

--- src/main/java/myproject/MyClass.java
+++ src/main/java/myproject/MyClass.java
@@ -7,4 +7,17 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 	}
+	
+	public void pull() {
+		System.out.println("Another chnags");
+		
+		
+		
+	}
+	public void middle() {
+		System.out.println("Somethisn middle");
+	}
+	public void pull1() {
+		System.out.println("Another changes");
+	}
 }
